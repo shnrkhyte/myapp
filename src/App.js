5f8aaf40ad596779b551ab102f97a4976cf849f2
EC2 Default User <ec2-user@ip-172-31-39-79.ap-northeast-1.compute.internal>
2020-04-29 07:15:07 +0000

    4

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import React from 'react';
 
 import Amplify from 'aws-amplify';
-import aws_exports from './aws-exports';
 
-import { withAuthenticator } from 'aws-amplify-react';
 import { Header } from 'semantic-ui-react';
 
-Amplify.configure(aws_exports);
 
 function App() {
   return (
@@ -16,10 +13,5 @@ function App() {
   );
 }
 
-export default withAuthenticator(App, {
-  includeGreetings: true,
-  signUpConfig: {
-    hiddenDefaults: ['phone_number']
-  }
-});
+export default App;
 
